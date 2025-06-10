c1c2d1912d8d9d15f9903613de00091060e4cfa8
Luís Arthur <luisarthurofficial@gmail.com>
2025-06-10 08:47:58 -0300

    fix: last level not calculating

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ let levelData = [
     ["Ferro", 1000], ["Bronze", 2000], ["Prata", 5000], ["Ouro", 7000], ["Platina", 8000], ["Ascendente", 9000], ["Imortal", 10000], ["Radiante", 10001]
 ]
 
-for(var l = 0; l < 7; l++){
+for(var l = 0; l < levelData.length; l++){
     if(experience <= levelData[l][1]){
         level = levelData[l][0];
         break;
     }
+}
+
+if (experience > levelData[levelData.length - 1][1]) {
+    level = levelData[levelData.length - 1][0];
 }
 
 console.log("O Héroi de nome " + nomeHeroi + " esta no nivel " + level)
